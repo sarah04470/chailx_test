c106c94ccd135d03ee4ace1b5c206631c460a8bb
seonha hwang <sarah04470@naver.com>
2025-03-03 12:07:29 +0900

    feat: board 페이지 section 2 추가 (PC 대응까지만 완료)

---
+++ src/components/sections/BoardList.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import chalixData from '@/data/chailx.json';
+import styles from '@/styles/BoardList.module.css';
+
+const BoardListSection = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(chalixData.list.slice(0, 10)); // 최대 10개만 가져오기
+  }, []);
+
+  return (
+    <section className={styles.board}>
+      <div className={styles['board-content']}>
+        <ul className={[styles['board-list'], 'grid-title'].join(' ')}>
+          <li className={styles['board-item']}>
+            <span className={styles['board-index']}>번호</span>
+            <span className={styles['board-conf']}>학술대회명</span>
+            <span className={styles['board-title']}>논문명</span>
+            <span className={styles['board-date']}>날짜</span>
+            <span className={styles['board-tag']}>비고(국내/해외)</span>
+          </li>
+        </ul>
+        <ul className={styles['board-list']}>
+          {data.map((item) => (
+            <li key={item.brd_idx} className={styles['board-item']}>
+              <span className={styles['board-index']}>{item.brd_idx}</span>
+              <span className={styles['board-conf']}>{item.brd_ext2}</span>
+              <span className={styles['board-title']}>{item.brd_title}</span>
+              <span className={styles['board-date']}>
+                {new Intl.DateTimeFormat('ko-kr', {
+                  year: 'numeric',
+                  month: '2-digit',
+                  day: '2-digit',
+                })
+                  .format(new Date(item.reg_datetime))
+                  .replace(/\. /g, '-')
+                  .replace('.', '')}
+              </span>
+              <span className={styles['board-tag']}>{item.brd_ext1}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default BoardListSection;
--- src/pages/board/Presentation.jsx
+++ src/pages/board/Presentation.jsx
@@ -1,13 +1,13 @@
 import styles from '@/styles/Presentation.module.css';
 import React from 'react';
 import HeroSection from '@sections/HeroSection';
-// import BoardListSection from '@sections/BoardList';
+import BoardListSection from '@sections/BoardList';
 
 const Presentation = () => {
   return (
     <main>
       <HeroSection />
-      {/* <BoardListSection /> */}
+      <BoardListSection />
     </main>
   );
 };

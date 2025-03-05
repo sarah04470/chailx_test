2a164d0fe601333c4801c5deda8804845b1d736a
seonha hwang <sarah04470@naver.com>
2025-03-05 16:32:17 +0900

    feat: board 섹션 모바일 추가

--- src/components/sections/BoardList.jsx
+++ src/components/sections/BoardList.jsx
@@ -15,19 +15,27 @@ const BoardListSection = () => {
         <ul className={[styles['board-list'], 'grid-title'].join(' ')}>
           <li className={styles['board-item']}>
             <span className={styles['board-index']}>번호</span>
-            <span className={styles['board-conf']}>학술대회명</span>
+            <span className={[styles['board-conf'], 'pc-only'].join(' ')}>
+              학술대회명
+            </span>
             <span className={styles['board-title']}>논문명</span>
-            <span className={styles['board-date']}>날짜</span>
-            <span className={styles['board-tag']}>비고(국내/해외)</span>
+            <span className={[styles['board-date'], 'pc-only'].join(' ')}>
+              날짜
+            </span>
+            <span className={[styles['board-tag'], 'pc-only'].join(' ')}>
+              비고(국내/해외)
+            </span>
           </li>
         </ul>
         <ul className={styles['board-list']}>
           {data.map((item) => (
             <li key={item.brd_idx} className={styles['board-item']}>
               <span className={styles['board-index']}>{item.brd_idx}</span>
-              <span className={styles['board-conf']}>{item.brd_ext2}</span>
+              <span className={[styles['board-conf'], 'pc-only'].join(' ')}>
+                {item.brd_ext2}
+              </span>
               <span className={styles['board-title']}>{item.brd_title}</span>
-              <span className={styles['board-date']}>
+              <span className={[styles['board-date'], 'pc-only'].join(' ')}>
                 {new Intl.DateTimeFormat('ko-kr', {
                   year: 'numeric',
                   month: '2-digit',
@@ -37,7 +45,9 @@ const BoardListSection = () => {
                   .replace(/\. /g, '-')
                   .replace('.', '')}
               </span>
-              <span className={styles['board-tag']}>{item.brd_ext1}</span>
+              <span className={[styles['board-tag'], 'pc-only'].join(' ')}>
+                {item.brd_ext1}
+              </span>
             </li>
           ))}
         </ul>
